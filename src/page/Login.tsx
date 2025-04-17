965c38ec3deca68dfacd68a99791b3a23613ba8d
GinoGallardo <ginosgt@gmail.com>
2025-04-17 09:06:09 -0300

    create: creacion de rutas para Login y Register

---
+++ src/page/Login.tsx
@@ -0,0 +1,10 @@
+const Login = () => {
+  return (
+    <>
+      <h2>Login</h2>
+      {/* Aquí iria el form de login */}
+    </>
+  );
+};
+
+export default Login;
---
+++ src/page/Register.tsx
@@ -0,0 +1,11 @@
+const Register = () => {
+  return (
+    <>
+      <h2>Regitro</h2>
+
+      {/* Aquí iria el form de registro */}
+    </>
+  );
+};
+
+export default Register;
