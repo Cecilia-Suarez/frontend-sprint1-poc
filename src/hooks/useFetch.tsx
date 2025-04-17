e415a1f7b9f21210e25d2c065378395899540822
Cecilia Suarez <cecisuarez08@gmail.com>
2025-04-17 13:19:25 -0300

    feat(hooks): add generic useFetch hook with error handling using React Query

---
+++ src/hooks/useFetch.tsx
@@ -0,0 +1,30 @@
+import { useQuery, UseQueryResult } from "@tanstack/react-query"
+import api from "../api/axios"
+
+const useFetch = <T>(key: string, endpoint: string): UseQueryResult <T> => {
+  const fetchData = async (): Promise<T> => {
+    try {
+      const { data } = await api.get<T>(endpoint);
+
+      if (!data) {
+        throw new Error("No data was received from the API");
+      }
+
+      console.log("Data received:", data);
+
+      return data;
+    } catch (error) {
+      console.error("Request error:", error);
+      throw new Error("Error fetching the data");
+    }
+  };
+
+  return useQuery<T, Error, T>({
+    queryKey: [key],
+    queryFn: fetchData,
+    staleTime: 1000 * 60 * 5,
+    retry: false,
+  });
+};
+
+export default useFetch;
