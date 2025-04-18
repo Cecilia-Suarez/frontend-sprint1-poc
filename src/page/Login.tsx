d4cc75501a6adb290ef036953157628409e4a0f2
Cristian Daniel Toia <toiacristiandaniel@gmail.com>
2025-04-18 16:36:16 -0300

    Creación de componente Notifications.tsx y Actualización de Register y Login

--- src/page/Login.tsx
+++ src/page/Login.tsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import { useForm } from "react-hook-form";
+import Notification from "../components/Notifications";
 
 interface LoginInputs {
   email: string;
@@ -11,15 +13,33 @@ const Login = () => {
     handleSubmit,
     formState: { errors },
   } = useForm<LoginInputs>();
+  const [notification, setNotification] = useState({
+    text: "",
+    type: "error" as "success" | "error",
+    show: false,
+  });
+
   const onSubmit = (data: LoginInputs): void => {
-    console.log("Datos de inicio de sesión:", data);
+    if (data.email === "test@example.com" && data.password === "password123") {
+      setNotification({
+        text: "Inicio de sesión exitoso.",
+        type: "success",
+        show: true,
+      });
+    } else {
+      setNotification({
+        text: "Revise sus credenciales.",
+        type: "error",
+        show: true,
+      });
+    }
   };
 
   return (
     <div className="flex bg-gray-900">
       <form
         onSubmit={(event) => {
-          handleSubmit(onSubmit)(event).catch((error: unknown) => {
+          void handleSubmit(onSubmit)(event).catch((error: unknown) => {
             if (error instanceof Error) {
               console.error("Error durante el submit:", error.message);
             } else {
@@ -27,52 +47,56 @@ const Login = () => {
             }
           });
         }}
-        className="bg-gray-800 text-white p-8 rounded-lg shadow-md w-full max-w-md"
+        className="w-full max-w-md rounded-lg bg-gray-800 p-8 text-white shadow-md"
       >
-        <h2 className="text-3xl font-bold text-center mb-6 text-indigo-400">
+        <h2 className="mb-6 text-center text-3xl font-bold text-indigo-400">
           Login para Músicos Emergentes
         </h2>
 
         <div className="mb-4">
-          <label htmlFor="email" className="block text-gray-300 mb-2">
+          <label htmlFor="email" className="mb-2 block text-gray-300">
             Correo Electrónico
           </label>
           <input
             type="email"
             id="email"
             {...register("email", { required: "El correo electrónico es obligatorio" })}
-            className="w-full border border-gray-700 p-2 rounded bg-gray-800 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+            className="w-full rounded border border-gray-700 bg-gray-800 p-2 focus:ring-2 focus:ring-indigo-500 focus:outline-none"
           />
-          {errors.email && (
-            <p className="text-red-500 mt-1">{errors.email.message}</p>
-          )}
+          {errors.email && <p className="mt-1 text-red-500">{errors.email.message}</p>}
         </div>
 
         <div className="mb-4">
-          <label htmlFor="password" className="block text-gray-300 mb-2">
+          <label htmlFor="password" className="mb-2 block text-gray-300">
             Contraseña
           </label>
           <input
             type="password"
             id="password"
             {...register("password", { required: "La contraseña es obligatoria" })}
-            className="w-full border border-gray-700 p-2 rounded bg-gray-800 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+            className="w-full rounded border border-gray-700 bg-gray-800 p-2 focus:ring-2 focus:ring-indigo-500 focus:outline-none"
           />
-          {errors.password && (
-            <p className="text-red-500 mt-1">{errors.password.message}</p>
-          )}
+          {errors.password && <p className="mt-1 text-red-500">{errors.password.message}</p>}
         </div>
 
         <button
           type="submit"
-          className="w-full py-2 px-4 bg-indigo-500 rounded text-white hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-700"
+          className="w-full rounded bg-indigo-500 px-4 py-2 text-white hover:bg-indigo-600"
         >
           Iniciar Sesión
         </button>
       </form>
+
+      <Notification
+        text={notification.text}
+        type={notification.type}
+        show={notification.show}
+        onClose={() => {
+          setNotification({ ...notification, show: false });
+        }}
+      />
     </div>
   );
 };
 
 export default Login;
-
--- src/page/Register.tsx
+++ src/page/Register.tsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import { useForm } from "react-hook-form";
+import Notification from "../components/Notifications";
 
 interface RegisterInputs {
   name: string;
@@ -14,16 +16,39 @@ const Register = () => {
     handleSubmit,
     formState: { errors },
   } = useForm<RegisterInputs>();
+  const [notification, setNotification] = useState({
+    text: "",
+    type: "error" as "success" | "error",
+    show: false,
+  });
 
   const onSubmit = (data: RegisterInputs): void => {
-    console.log("Datos del músico registrado:", data);
+    if (data.email === "registrado@example.com") {
+      setNotification({
+        text: "Email ya registrado!!.",
+        type: "error",
+        show: true,
+      });
+    } else if (!data.name || !data.password || !data.email) {
+      setNotification({
+        text: "Revise los campos!!.",
+        type: "error",
+        show: true,
+      });
+    } else {
+      setNotification({
+        text: "Registrado con Ëxito!!.",
+        type: "success",
+        show: true,
+      });
+    }
   };
 
   return (
-    <div className="flex bg-gray-900 ">
+    <div className="flex bg-gray-900">
       <form
         onSubmit={(event) => {
-          handleSubmit(onSubmit)(event).catch((error: unknown) => {
+          void handleSubmit(onSubmit)(event).catch((error: unknown) => {
             if (error instanceof Error) {
               console.error("Error durante el submit:", error.message);
             } else {
@@ -31,88 +56,93 @@ const Register = () => {
             }
           });
         }}
-        className="bg-gray-800 text-white p-8 rounded-lg shadow-md w-full max-w-lg"
+        className="w-full max-w-lg rounded-lg bg-gray-800 p-8 text-white shadow-md"
       >
-        <h2 className="text-3xl font-bold text-center mb-6 text-indigo-400">
-          Registro para Músicos Emergentes
+        <h2 className="mb-6 text-center text-3xl font-bold text-indigo-400">
+          Registrate y que el Mundo te Escuche!!
         </h2>
 
         <div className="mb-4">
-          <label htmlFor="name" className="block text-gray-300 mb-2">
+          <label htmlFor="name" className="mb-2 block text-gray-300">
             Nombre Artístico
           </label>
           <input
             type="text"
             id="name"
             {...register("name", { required: "El nombre artístico es obligatorio" })}
-            className="w-full border border-gray-700 p-2 rounded bg-gray-800 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+            className="w-full rounded border border-gray-700 bg-gray-800 p-2 focus:ring-2 focus:ring-indigo-500 focus:outline-none"
           />
-          {errors.name && (
-            <p className="text-red-500 mt-1">{errors.name.message}</p>
-          )}
+          {errors.name && <p className="mt-1 text-red-500">{errors.name.message}</p>}
         </div>
 
         <div className="mb-4">
-          <label htmlFor="email" className="block text-gray-300 mb-2">
+          <label htmlFor="email" className="mb-2 block text-gray-300">
             Correo Electrónico
           </label>
           <input
             type="email"
             id="email"
             {...register("email", { required: "El correo electrónico es obligatorio" })}
-            className="w-full border border-gray-700 p-2 rounded bg-gray-800 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+            className="w-full rounded border border-gray-700 bg-gray-800 p-2 focus:ring-2 focus:ring-indigo-500 focus:outline-none"
           />
-          {errors.email && (
-            <p className="text-red-500 mt-1">{errors.email.message}</p>
-          )}
+          {errors.email && <p className="mt-1 text-red-500">{errors.email.message}</p>}
         </div>
 
         <div className="mb-4">
-          <label htmlFor="password" className="block text-gray-300 mb-2">
+          <label htmlFor="password" className="mb-2 block text-gray-300">
             Contraseña
           </label>
           <input
             type="password"
             id="password"
             {...register("password", { required: "La contraseña es obligatoria" })}
-            className="w-full border border-gray-700 p-2 rounded bg-gray-800 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+            className="w-full rounded border border-gray-700 bg-gray-800 p-2 focus:ring-2 focus:ring-indigo-500 focus:outline-none"
           />
-          {errors.password && (
-            <p className="text-red-500 mt-1">{errors.password.message}</p>
-          )}
+          {errors.password && <p className="mt-1 text-red-500">{errors.password.message}</p>}
         </div>
 
+        {/* Campo opcional: Género Musical */}
         <div className="mb-4">
-          <label htmlFor="genero" className="block text-gray-300 mb-2">
+          <label htmlFor="genero" className="mb-2 block text-gray-300">
             Género Musical (Opcional)
           </label>
           <input
             type="text"
             id="genero"
             {...register("genero")}
-            className="w-full border border-gray-700 p-2 rounded bg-gray-800 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+            className="w-full rounded border border-gray-700 bg-gray-800 p-2 focus:ring-2 focus:ring-indigo-500 focus:outline-none"
           />
         </div>
 
+        {/* Campo opcional: Años de Experiencia */}
         <div className="mb-4">
-          <label htmlFor="experience" className="block text-gray-300 mb-2">
+          <label htmlFor="experience" className="mb-2 block text-gray-300">
             Años de Experiencia (Opcional)
           </label>
           <input
             type="number"
             id="experience"
             {...register("experience", { valueAsNumber: true })}
-            className="w-full border border-gray-700 p-2 rounded bg-gray-800 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+            className="w-full rounded border border-gray-700 bg-gray-800 p-2 focus:ring-2 focus:ring-indigo-500 focus:outline-none"
           />
         </div>
 
         <button
           type="submit"
-          className="w-full py-2 px-4 bg-indigo-500 rounded text-white hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-700"
+          className="w-full rounded bg-indigo-500 px-4 py-2 text-white hover:bg-indigo-600 focus:ring-2 focus:ring-indigo-700 focus:outline-none"
         >
           Registrarse
         </button>
       </form>
+
+      <Notification
+        text={notification.text}
+        type={notification.type}
+        show={notification.show}
+        onClose={() => {
+          setNotification({ ...notification, show: false });
+        }}
+      />
     </div>
   );
 };
