a96c52ca728687ae96bf9d2af52af63951ea6840
Nicolas Pedranti <nicopedranti@gmail.com>
2025-04-24 00:09:20 -0300

    refactor: add zod schema and resolvers for react-hook-form

--- src/page/Register.tsx
+++ src/page/Register.tsx
@@ -1,62 +1,81 @@
-import { useState } from "react";
-import { useForm } from "react-hook-form";
-import Notification from "../components/Notifications";
+// import { useState } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { register as authRegister } from "../api/backend";
+// import Notification from "../components/Notifications";
 
-interface RegisterInputs {
-  name: string;
-  email: string;
-  password: string;
-  genero?: string; // Opcional
-  experience?: number; // Opcional
-}
+const registerSchema = z.object({
+  name: z.string().min(3, { message: "Nombre obligatorio" }),
+  lastName: z.string().min(3, { message: "Apellido obligatorio" }),
+  email: z.string().email({ message: "Email no valido" }),
+  password: z.string().min(8, { message: "Tu contraseña debe tener al menos 8 caracteres" }),
+  role: z.enum(["ROLE_USER", "ROLE_ADMIN"]),
+});
+
+export type FormFields = z.infer<typeof registerSchema>;
 
 const Register = () => {
   const {
     register,
     handleSubmit,
+    setError,
     formState: { errors, isSubmitting },
-  } = useForm<RegisterInputs>();
-  const [notification, setNotification] = useState({
-    text: "",
-    type: "error" as "success" | "error",
-    show: false,
+  } = useForm<FormFields>({
+    resolver: zodResolver(registerSchema),
   });
 
-  const onSubmit = (data: RegisterInputs): void => {
-    if (data.email === "registrado@example.com") {
-      setNotification({
-        text: "Email ya registrado!!.",
-        type: "error",
-        show: true,
-      });
-    } else if (!data.name || !data.password || !data.email) {
-      setNotification({
-        text: "Revise los campos!!.",
-        type: "error",
-        show: true,
-      });
-    } else {
-      setNotification({
-        text: "Registrado con Ëxito!!.",
-        type: "success",
-        show: true,
+  // const [notification, setNotification] = useState({
+  //   text: "",
+  //   type: "error" as "success" | "error",
+  //   show: false,
+  // });
+
+  const onSubmit: SubmitHandler<FormFields> = async (formData: FormFields) => {
+    // if (data.email === "registrado@example.com") {
+    //   setNotification({
+    //     text: "Email ya registrado!!.",
+    //     type: "error",
+    //     show: true,
+    //   });
+    // } else if (!data.name || !data.password || !data.email) {
+    //   setNotification({
+    //     text: "Revise los campos!!.",
+    //     type: "error",
+    //     show: true,
+    //   });
+    // } else {
+    //   setNotification({
+    //     text: "Registrado con Ëxito!!.",
+    //     type: "success",
+    //     show: true,cls
+
+    //   });
+    // }
+    console.log("FORM VALUES --> ", formData);
+    console.log("ENVIANDO AL SERVIDOR...");
+
+    try {
+      const { data, status } = await authRegister(formData);
+      console.log("DATA DEL SERVIDOR LOGINFORM --> ", data);
+      console.log("STATUS DEL SERVIDOR LOGINFORM --> ", status);
+
+      if (!data) {
+        throw new Error("Error al intentar ingresar");
+      }
+    } catch (error) {
+      console.error(error);
+      setError("root", {
+        message: error as string,
       });
     }
   };
 
   return (
-    <div className="flex bg-gray-900">
+    <div className="bg-gray-900">
       <form
-        onSubmit={(event) => {
-          void handleSubmit(onSubmit)(event).catch((error: unknown) => {
-            if (error instanceof Error) {
-              console.error("Error durante el submit:", error.message);
-            } else {
-              console.error("Error durante el submit:", error);
-            }
-          });
-        }}
-        className="w-full max-w-lg rounded-lg bg-gray-800 p-8 text-white shadow-md"
+        onSubmit={handleSubmit(onSubmit)}
+        className="max-w-lg rounded-lg bg-gray-800 p-8 text-white shadow-md"
       >
         <h2 className="mb-6 text-center text-3xl font-bold text-indigo-400">
           Registrate y que el Mundo te Escuche!!
@@ -64,28 +83,38 @@ const Register = () => {
 
         <div className="mb-4">
           <label htmlFor="name" className="mb-2 block text-gray-300">
-            Nombre Artístico
+            Nombre
           </label>
           <input
             type="text"
-            id="name"
-            {...register("name", { required: "El nombre artístico es obligatorio" })}
+            {...register("name")}
             className="w-full rounded border border-gray-700 bg-gray-800 p-2 focus:ring-2 focus:ring-indigo-500 focus:outline-none"
           />
-          {errors.name && <p className="mt-1 text-red-500">{errors.name.message}</p>}
+          {errors.name && <p className="mt-1 h-6 text-red-500">{errors.name.message}</p>}
+        </div>
+
+        <div className="mb-4">
+          <label htmlFor="lastName" className="mb-2 block text-gray-300">
+            Apellido
+          </label>
+          <input
+            type="text"
+            {...register("lastName")}
+            className="w-full rounded border border-gray-700 bg-gray-800 p-2 focus:ring-2 focus:ring-indigo-500 focus:outline-none"
+          />
+          {errors.lastName && <p className="mt-1 h-6 text-red-500">{errors.lastName.message}</p>}
         </div>
 
         <div className="mb-4">
           <label htmlFor="email" className="mb-2 block text-gray-300">
-            Correo Electrónico
+            Email
           </label>
           <input
             type="email"
-            id="email"
-            {...register("email", { required: "El correo electrónico es obligatorio" })}
+            {...register("email")}
             className="w-full rounded border border-gray-700 bg-gray-800 p-2 focus:ring-2 focus:ring-indigo-500 focus:outline-none"
           />
-          {errors.email && <p className="mt-1 text-red-500">{errors.email.message}</p>}
+          {errors.email && <p className="mt-1 h-6 text-red-500">{errors.email.message}</p>}
         </div>
 
         <div className="mb-4">
@@ -94,37 +123,21 @@ const Register = () => {
           </label>
           <input
             type="password"
-            id="password"
-            {...register("password", { required: "La contraseña es obligatoria" })}
+            {...register("password")}
             className="w-full rounded border border-gray-700 bg-gray-800 p-2 focus:ring-2 focus:ring-indigo-500 focus:outline-none"
           />
-          {errors.password && <p className="mt-1 text-red-500">{errors.password.message}</p>}
+          {errors.password && <p className="mt-1 h-6 text-red-500">{errors.password.message}</p>}
         </div>
 
-        {/* Campo opcional: Género Musical */}
         <div className="mb-4">
-          <label htmlFor="genero" className="mb-2 block text-gray-300">
-            Género Musical (Opcional)
+          <label htmlFor="role" className="mb-2 block text-gray-300">
+            Rol
           </label>
-          <input
-            type="text"
-            id="genero"
-            {...register("genero")}
-            className="w-full rounded border border-gray-700 bg-gray-800 p-2 focus:ring-2 focus:ring-indigo-500 focus:outline-none"
-          />
-        </div>
-
-        {/* Campo opcional: Años de Experiencia */}
-        <div className="mb-4">
-          <label htmlFor="experience" className="mb-2 block text-gray-300">
-            Años de Experiencia (Opcional)
-          </label>
-          <input
-            type="number"
-            id="experience"
-            {...register("experience", { valueAsNumber: true })}
-            className="w-full rounded border border-gray-700 bg-gray-800 p-2 focus:ring-2 focus:ring-indigo-500 focus:outline-none"
-          />
+          <select {...register("role")}>
+            <option value="ROLE_USER">Usuario</option>
+            <option value="ROLE_ADMIN">Admin</option>
+          </select>
+          {errors.role && <p className="mt-1 h-1 text-red-500">{errors.role.message}</p>}
         </div>
 
         <button
@@ -132,18 +145,19 @@ const Register = () => {
           disabled={isSubmitting}
           className="w-full rounded bg-indigo-500 px-4 py-2 text-white hover:bg-indigo-600 focus:ring-2 focus:ring-indigo-700 focus:outline-none"
         >
-          {isSubmitting ? "Cargando..." : "Registrarse"}
+          {isSubmitting ? "Registrandose..." : "Registrarse"}
         </button>
+        {errors.root && <div className="text-red-500">{errors.root.message}</div>}
       </form>
 
-      <Notification
+      {/* <Notification
         text={notification.text}
         type={notification.type}
         show={notification.show}
         onClose={() => {
           setNotification({ ...notification, show: false });
         }}
-      />
+      /> */}
     </div>
   );
 };
