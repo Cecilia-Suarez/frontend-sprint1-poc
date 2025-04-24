a7c51ab7cc0d90fcf13d2dbc0856015b7fe37ec9
Nicolas Pedranti <nicopedranti@gmail.com>
2025-04-24 00:04:36 -0300

    chore: delete unused components from Home

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,13 +1,9 @@
 import Albums from "../components/Albums";
-import LoginTest from "../test/loginTest";
-import RegisterTest from "../test/registerTest";
 
 const Home = () => {
   return (
     <div>
       <Albums />
-      <RegisterTest />
-      <LoginTest />
     </div>
   );
 };
