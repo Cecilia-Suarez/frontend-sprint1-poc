2d31ea87aaa33e602246cb88eb74d0566cf2a111
GinoGallardo <ginosgt@gmail.com>
2025-04-25 01:11:35 -0300

    Fix: corrección de error en modal y creación de nuevo form

--- src/validation/ProductTableSchema.ts
+++ src/validation/ProductTableSchema.ts
@@ -1,7 +1,15 @@
 import { z } from "zod";
 
-export const ProductTableSchema = z.object({
+export const ProductAddTableSchema = z.object({
   id: z.number().optional(),
+  title: z.string().min(3, "El titulo debe de tener al menos 3 caracteres"),
+  price: z.coerce.number().min(3, "El precio debe ser mayor o igual a 0"),
+  description: z.string().min(3, "La descripción debe de tener al menos 3 caracteres"),
+  category: z.string().min(3, "La categoría debe de tener al menos 3 caracteres"),
+});
+
+export const ProductEditTableSchema = z.object({
+  id: z.number(),
   title: z.string().min(3, "El titulo debe de tener al menos 3 caracteres"),
   price: z.coerce.number().min(3, "El precio debe ser mayor o igual a 0"),
   description: z.string().min(3, "La descripción debe de tener al menos 3 caracteres"),
